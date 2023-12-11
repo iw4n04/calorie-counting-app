12d309d3893d8498a867730bde847beccc76e0ea
iw4n04 <iwaniwaniwan04@yandex.ru>
2023-12-12 02:46:41 +0700

    1

--- app/src/main/java/com/example/myapplication/db/dataBaseClass.java
+++ app/src/main/java/com/example/myapplication/db/dataBaseClass.java
@@ -1,19 +1,12 @@
 package com.example.myapplication.db;
-
-import android.provider.BaseColumns;
-
-public class dataBaseClass implements BaseColumns {
+public class dataBaseClass {
     public static final String TABLE_NAME = "day_calloriy";
+    public static final String _ID = "id";
     public static final String DAY = "day";
     public static final String COLUMN_NAME_PRODUCT = "product";
     public static final String COLUMN_NAME_TIME = "time";
     public static final String COLUMN_NAME_CALORIES = "calories";
-    public static final String TABLE_STRUCTURE = "CREATE TABLE IF NOT EXISTS" +
-            TABLE_NAME + "(" +
-            _ID + "INTEGER PRIMARY KEY," +
-            DAY + "TEXT," +
-            COLUMN_NAME_PRODUCT + "TEXT," +
-            COLUMN_NAME_TIME + "TEXT," +
-            COLUMN_NAME_CALORIES + "INTEGER PRIMARY KEY)";
+    public static final String TABLE_STRUCTURE = "CREATE TABLE IF NOT EXISTS day_calloriy " +
+            "(id INTEGER PRIMARY KEY,day DATE,product TEXT,time TEXT,calories INTEGER PRIMARY KEY)";
     public static final String SQL_DELETE_DATA_BASE = "DROP TABLE IF EXISTS" + TABLE_NAME;
 }
