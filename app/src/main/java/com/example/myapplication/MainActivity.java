401bfd69d13677888a8a7beae17f103425ff6ab7
iw4n04 <iwaniwaniwan04@yandex.ru>
2023-12-12 10:21:06 +0700

    1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,13 +3,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CalendarView;
 
-import com.example.myapplication.db.myDBManager;
-
 public class MainActivity extends AppCompatActivity {
-    private myDBManager myDBManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/myapplication/spisokProduktov.java
+++ app/src/main/java/com/example/myapplication/spisokProduktov.java
@@ -2,35 +2,34 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import com.example.myapplication.db.myDBManager;
+
+import com.example.myapplication.db.DBHandler;
+import com.example.myapplication.db.dbMeal;
+
+import java.util.List;
+
 public class spisokProduktov extends AppCompatActivity {
     private TextView textView4;
-    private myDBManager myDBManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spisok_produktov);
-        textView4 = findViewById(R.id.textView4);
-        myDBManager = new myDBManager(this);
+        textView4 = findViewById(R.id.textView4);;
+        DBHandler DBHandler = new DBHandler(this);
+        DBHandler.addDBMeal(new dbMeal("jkml", "jjnkjn", 464, ""));
+        List<dbMeal> mealList = DBHandler.getAllMeal();
+        for (dbMeal dbMeal : mealList)
+        {
+            Log.d("Meal info: ", "name " + dbMeal.getName() + " , time - " +
+                    dbMeal.getTime() + " , calories" + dbMeal.getCalories());
+        }
     }
     public void goToMainActivity(View v)
     {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-    }
-    public void onClickShowDB(View view) {
-        myDBManager.openDB();
-        for (String title : myDBManager.getFromDB())
-        {
-            textView4.append(title);
-            textView4.append("\n");
-        }
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        myDBManager.closeDB();
     }
 }
